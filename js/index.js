aee08d7ba02704e05e72b5d9166099c1cc92b1c2
D-Hussainn <302984963@student.rocmondriaan.nl>
2024-06-13 19:55:03 +0200

    alles in bootstrap

--- js/index.js
+++ js/index.js
@@ -1,32 +1,45 @@
-const Logo = document.querySelector(".logo");
-const GoToHome = document.querySelector(".home-button");
-const GoToOverMij = document.querySelector(".over-mij-button");
-const GoToProject = document.querySelector(".project-button");
-const GoToMijnScooter = document.querySelector(".scooter-button");
-const GoToSocial = document.querySelector(".socials-footer");
+window.addEventListener('DOMContentLoaded', event => {
 
-// button isn header
-Logo.addEventListener("click", () => {
-    window.location.href = "/index.html";
+    // Navbar shrink function
+    let navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
 });
-
-GoToHome.addEventListener("click", () => {
-    window.location.href = "/index.html";
-});
-
-GoToOverMij.addEventListener("click", () => {
-    window.location.href = "/html/over-mij.html";
-});
-
-GoToProject.addEventListener("click", () => {
-    window.location.href = "/html/project.html";
-});
-
-GoToMijnScooter.addEventListener("click", () => {
-    window.location.href = "/html/mijn-scooter.html";
-});
-
-GoToSocial.addEventListener("click", () => {
-    window.location.href = "/html/over-mij.html";
-});
-
